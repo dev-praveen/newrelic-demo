73b71c6099babd6f8263e88c7944fd961662d068
dev-praveen <spraveen0244@gmail.com>
2026-06-20 12:35:31 +0530

    initial commit

---
+++ src/main/java/com/praveen/learn/NewrelicDemoApplication.java
@@ -0,0 +1,12 @@
+package com.praveen.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewrelicDemoApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NewrelicDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/praveen/learn/NewrelicDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.praveen.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewrelicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
